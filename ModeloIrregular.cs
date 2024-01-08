6a5b6277c0bce551a21384c6bbfeba87f3abc3fb
Charly Manrique <charly.manrique92@gmail.com>
2024-01-07 20:03:56 -0500

    Se elimino código comentado y se corrigieron errores de ortografia

---
+++ ModeloIrregular.cs
@@ -0,0 +1,11 @@
+public class ModeloIrregular
+{
+    public int Consecutivo { get; set; }
+    public string Forma_base { get; set; }
+
+    public string Preterito { get; set; }
+
+    public string Pasado_participio { get; set; }
+
+    public string Traduccion { get; set; }
+}
